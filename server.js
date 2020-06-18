2808cea51ddbd73178534bff437e5e8fdffef44d
= <=>
2020-06-18 02:52:35 +0100

    geggity giggity goo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-
+//server all
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
